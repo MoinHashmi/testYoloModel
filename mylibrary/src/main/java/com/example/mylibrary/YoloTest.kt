70834d00177478c75d33dd1415049909af465c88
moin hashmi <moin.hashmi@analytics.com.pk>
2020-11-02 14:46:59 +0500

    path change to "mylibrary/src/main/assets"

--- mylibrary/src/main/java/com/example/mylibrary/YoloTest.kt
+++ mylibrary/src/main/java/com/example/mylibrary/YoloTest.kt
@@ -13,7 +13,33 @@ object YoloTest {
 
     fun getModelPath(context: Context): String {
         val asset: String="best_yolov5s_small_torchscript.pt"
-        return "file:///android_asset/"+asset;
+
+        val file = File(context.filesDir, asset)
+
+        try {
+            val inpStream: InputStream = context.assets.open(asset)
+            val outStream = FileOutputStream(file, false)
+            val buffer = ByteArray(4 * 1024)
+            var read: Int
+
+            while (true) {
+                read = inpStream.read(buffer)
+                if (read == -1) {
+                    break
+                }
+                outStream.write(buffer, 0, read)
+            }
+            outStream.flush()
+            return file.absolutePath
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return "mylibrary/src/main/assets/$asset"
     }
+
+//    fun getModel(context: Context): InputStream {
+//        val am = context.assets
+//        return am.open("best_yolov5s_small_torchscript.pt")
+//    }
 
 }
