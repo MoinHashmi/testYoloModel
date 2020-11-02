3a5a610650e274292e2d38be70f3333c071927b9
moin hashmi <moin.hashmi@analytics.com.pk>
2020-11-02 13:09:35 +0500

    library added

--- app/src/main/java/com/example/yololibrary/MainActivity.java
+++ app/src/main/java/com/example/yololibrary/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.mylibrary.YoloTest;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("AssetPathIs","path: "+YoloTest.INSTANCE.getModelPath(this));
+        textView=findViewById(R.id.textView);
+        String strPath=YoloTest.INSTANCE.getModelPath(this);
+
+        textView.setText("path: "+strPath);
+
+//        Log.i("AssetPathIs","path: "+YoloTest.INSTANCE.getModelPath(this));
 
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/YoloTest.kt
+++ mylibrary/src/main/java/com/example/mylibrary/YoloTest.kt
@@ -14,27 +14,27 @@ object YoloTest {
     fun getModelPath(context: Context): String {
         val asset: String="best_yolov5s_small_torchscript.pt"
 
-        val file = File(context.filesDir, asset)
-
-        try {
-            val inpStream: InputStream = context.assets.open(asset)
-            val outStream = FileOutputStream(file, false)
-            val buffer = ByteArray(4 * 1024)
-            var read: Int
-
-            while (true) {
-                read = inpStream.read(buffer)
-                if (read == -1) {
-                    break
-                }
-                outStream.write(buffer, 0, read)
-            }
-            outStream.flush()
-            return file.absolutePath
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return ""
+//        val file = File(context.filesDir, asset)
+//
+//        try {
+//            val inpStream: InputStream = context.assets.open(asset)
+//            val outStream = FileOutputStream(file, false)
+//            val buffer = ByteArray(4 * 1024)
+//            var read: Int
+//
+//            while (true) {
+//                read = inpStream.read(buffer)
+//                if (read == -1) {
+//                    break
+//                }
+//                outStream.write(buffer, 0, read)
+//            }
+//            outStream.flush()
+//            return file.absolutePath
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+        return "file:///android_asset/"+asset;
     }
 
 //    fun getModel(context: Context): InputStream {
